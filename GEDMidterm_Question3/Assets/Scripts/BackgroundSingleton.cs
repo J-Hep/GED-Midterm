8433bc93975a18f003ea56268020cfcb72259dc8
JH <jaden.hepburn@ontariotechu.net>
2022-10-05 13:43:44 -0400

    Question3 ya

---
+++ GEDMidterm_Question3/Assets/Scripts/BackgroundSingleton.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Used from slides Lecture 2 page 99 link: http://www.unitygeek.com/unity_c_singleton/ 
+public class GenericSingletonClass<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+public class BackgroundSingleton: GenericSingletonClass<BackgroundSingleton>
+{
+  public GameObject backgroundElement, scoring;
+    
+
+    void Update()
+    {
+
+        //CODE IS CALLED ONCE AND DOES NOT INCREMENT. So It should show 6501 then finish.
+        int score = 6500;
+        
+
+        if (score >= 5000)
+        {
+            backgroundElement.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+        }
+
+        score++;
+        print("Score: "+score);
+    }
+
+}
+
+
